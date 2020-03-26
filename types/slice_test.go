f8f67b245102e80d89991893e20e412afebddb88
adevjoe <adevjoe@gmail.com>
2020-03-26 22:08:45 +0800

    slice test

--- types/slice_test.go
+++ types/slice_test.go
@@ -3,6 +3,7 @@ package types
 import (
 	"fmt"
 	"testing"
+	"unsafe"
 )
 
 func TestCovert(t *testing.T) {
@@ -11,4 +12,23 @@ func TestCovert(t *testing.T) {
 	fmt.Println("s1:", s1, "s2:", s2)
 	Covert(s1, s2)
 	fmt.Println("s1:", s1, "s2:", s2)
+}
+
+func TestCopy(t *testing.T) {
+	a := []int64{4, 2, 1}
+
+	b := make([]int64, len(a))
+	copy(b, a)
+
+	c := append([]int64{}, a...)
+
+	d := [3]int64{4, 2, 1}
+
+	e := []byte("hello")
+	copy(e[2:], e)
+	fmt.Printf("a: %v, point: %p, size: %v\n", a, a, unsafe.Sizeof(a))
+	fmt.Printf("b: %v, point: %p, size: %v\n", b, b, unsafe.Sizeof(b))
+	fmt.Printf("c: %v, point: %p, size: %v\n", c, c, unsafe.Sizeof(c))
+	fmt.Printf("d: %v, point: %p, size: %v\n", d, &d, unsafe.Sizeof(d))
+	fmt.Printf("e: %s, point: %p, size: %v\n", e, e, unsafe.Sizeof(e))
 }
