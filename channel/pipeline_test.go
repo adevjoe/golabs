3eec9e0385c1344f22de34d2f48a08222c00931d
adevjoe <adevjoe@gmail.com>
2021-09-18 17:43:27 +0800

    benchmark pipeline

--- channel/pipeline_test.go
+++ channel/pipeline_test.go
@@ -1,11 +1,41 @@
 package channel
 
 import (
-	"fmt"
 	"testing"
+	"time"
 )
 
 func TestPipeline(t *testing.T) {
+	pipelineFn(nil)
+	normalPipeline(nil)
+
+}
+
+func normalPipeline(fn func()) {
+	add := func(s []int, value int) []int {
+		for i := range s {
+			if fn != nil {
+				fn()
+			}
+			s[i] += value
+		}
+		return s
+	}
+	multiply := func(s []int, value int) []int {
+		for i := range s {
+			if fn != nil {
+				fn()
+			}
+			s[i] *= value
+		}
+		return s
+	}
+
+	a := []int{1, 2, 3, 4}
+	multiply(add(multiply(a, 2), 1), 2)
+}
+
+func pipelineFn(fn func()) {
 	generator := func(done <-chan interface{}, integers ...int) <-chan int {
 		intStream := make(chan int)
 		go func() {
@@ -30,6 +60,9 @@ func TestPipeline(t *testing.T) {
 		go func() {
 			defer close(multipliedStream)
 			for i := range intStream {
+				if fn != nil {
+					fn()
+				}
 				select {
 				case <-done:
 					return
@@ -49,6 +82,9 @@ func TestPipeline(t *testing.T) {
 		go func() {
 			defer close(addedStream)
 			for i := range intStream {
+				if fn != nil {
+					fn()
+				}
 				select {
 				case <-done:
 					return
@@ -62,10 +98,27 @@ func TestPipeline(t *testing.T) {
 	done := make(chan interface{})
 	defer close(done)
 
-	intStream := generator(done, 1, 2, 3, 4)
+	s := []int{1, 2, 3, 4}
+	intStream := generator(done, s...)
 	pipeline := multiply(done, add(done, multiply(done, intStream, 2), 1), 2)
-
+	b := make([]int, len(s))
 	for v := range pipeline {
-		fmt.Println(v)
+		b = append(b, v)
+	}
+}
+
+func BenchmarkPipeline(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		pipelineFn(func() {
+			time.Sleep(10 * time.Millisecond)
+		})
+	}
+}
+
+func BenchmarkNormalPipeline(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		normalPipeline(func() {
+			time.Sleep(10 * time.Millisecond)
+		})
 	}
 }
